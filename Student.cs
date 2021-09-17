ec0e41ada992c5ca9c45670fd8fcd7fecf07188e
Arief Samuel <ariefsamuel21@gmail.com>
2021-09-17 18:22:16 +0700

    initial commit

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        public float AvgGrade { get; set; }
+        public string GradeLetter { get; set; }
+    }
+}
